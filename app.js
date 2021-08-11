42466ba655cf408b0d17dbfc38c6d4bce96b3cf8
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-08-11 16:42:24 -0700

    fixing and adding buttons

--- app.js
+++ app.js
@@ -3,18 +3,26 @@ const nameInput = document.getElementById('input-name')
 const nameButton = document.getElementById('name-button')
 const magicalButton = document.getElementById('magical-button')
 const dreamyButton = document.getElementById('dreamy-button')
-const tagBackground = document.getElementById('tag')
+const fontButton = document.getElementById('font-button')
+const tag = document.getElementById('tag')
 
 
 nameButton.addEventListener('click', () => {
     myName.textContent = nameInput.value
 })
+
+fontButton.addEventListener('click', () => {
+    tag.style.fontFamily = 'monospace';
+    console.log ('we clicked the font button');
+})
+
 magicalButton.addEventListener('click', () => {
-    tagBackground.style.backgroundColor = 'purple';
+    tag.style.backgroundColor = 'purple';
     myName.style.color = "purple";
+
 })
 
 dreamyButton.addEventListener('click', () => {
-    tagBackground.style.backgroundImage = "url('clouds.jpg')";
+    tag.style.backgroundImage = "url('clouds.jpg')";
     myName.style.color = "pink";
 })
